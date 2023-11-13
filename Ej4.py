2732dd019f9d2ddab2698b8abfe8caf8c10f8176
María Marta Ramírez Gil <ram21342@uvg.edu.gt>
2023-11-13 02:29:19 -0600

    Update Ej4.py
    Ej4 terminado en python

---
+++ Ej4.py
@@ -0,0 +1,11 @@
+# Lista inicial
+lista_inicial = ['rojo', 'verde', 'azul', 'amarillo', 'gris', 'blanco', 'negro']
+
+# Lista de elementos a borrar
+elementos_a_borrar = ['amarillo', 'café', 'blanco']
+
+# Eliminar elementos indicados usando funciones lambda
+lista_resultante = list(filter(lambda x: x not in elementos_a_borrar, lista_inicial))
+
+# Imprimir la lista resultante
+print(lista_resultante)
